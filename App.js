4eca35967eeeba58613c2b04872d52b9a2e9610a
Ravi Kuchhal <ravi.kuchhal@timesgroup.com>
2025-07-17 04:26:52 +0530

    Episode-3

--- App.js
+++ App.js
@@ -19,33 +19,52 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
 
- const heading = React.createElement(
-    "h1",
-    {id:"heading",key:"firstChild"},
-    "Hello World using React!!")
+//  const heading = React.createElement(
+//     "h1",
+//     {id:"heading",key:"firstChild"},
+//     "Hello World using React!!")
 
-const parent = React.createElement(
-    "div",
-    {id:"parent"},
-    React.createElement(
-        "div",
-        {id:"child",key:"firstChild1"},
-        [
-            React.createElement(
-                "h1",
-                {id:"heading",key:"firstChild2"},
-                "I am H1 Tag!!!"
-            ),
-            React.createElement(
-                "h2",
-                {id:"heading2",key:"firstChild3"},  
-                "I am H2 Tag!!!"
-            )
-        ]
+// const parent = React.createElement(
+//     "div",
+//     {id:"parent"},
+//     React.createElement(
+//         "div",
+//         {id:"child",key:"firstChild1"},
+//         [
+//             React.createElement(
+//                 "h1",
+//                 {id:"heading",key:"firstChild2"},
+//                 "I am H1 Tag!!!"
+//             ),
+//             React.createElement(
+//                 "h2",
+//                 {id:"heading2",key:"firstChild3"},  
+//                 "I am H2 Tag!!!"
+//             )
+//         ]
         
-        )
-    )
-console.log('parent',parent);
+//         )
+//     )
+// console.log('parent',parent);
+
+
+ /** JSX - HTML Like Syntax */
+
+ const jsxHeading = <h1 id="heading">Hi Ravi !! This heading is using JSX</h1>
+
  const root = ReactDOM.createRoot(document.getElementById("root"));
  // const root = ReactDOM.createRoot("root");
- root.render(parent);
+
+ const Title = () => (
+    <h1>This is Title component!!</h1>
+ );
+
+ const HeadingComponent = () => (
+    <div>
+        <Title />
+        <h1>This is Heading component ie functional component</h1>
+    </div>
+ );
+
+
+ root.render(<HeadingComponent/>);
--- readMe.md
+++ readMe.md
@@ -8,4 +8,14 @@
 3- HMR
 4- File Watching Algorithm
 5- Caching - faster build
-6-   
+6- Image Optimizations
+7- Minification of file
+8- Bundling
+9- Compressing
+10- Code Spliting
+11- Differenetial Bundling - like app runs on my browser, it create differential bundling - older browser
+12- Diagonisitic
+13- Error Handling
+14- HTTPs
+15- Tree Shaking
+16- Different dev and prod bundles
