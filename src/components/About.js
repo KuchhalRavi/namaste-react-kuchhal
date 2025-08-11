fd87a57475a7735ca3e97738369517e4360b1310
Ravi Kuchhal <ravi.kuchhal@timesgroup.com>
2025-08-11 23:21:40 +0530

    episode-08 let get classy

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,48 @@
-const About = () => {
-return <h1>This is About Page</h1>
+import React from "react";
+import User from "./User";
+import UserClass from "./UserClass";
+// const About = () => {
+
+// return (
+//     <div className="about">
+//         <h1>This is About Page</h1>
+//         <User name="Ravi Kuchhal" city="Noida"/>
+//         <UserClass name="Ravi Kuchhal" city="Noida" />
+//     </div>
+    
+
+//     );
+// }
+
+/** Class based component */
+class About extends React.Component{
+    constructor(props)
+    {
+        super(props);
+        // console.log('parent constructor')
+    }
+
+    componentDidMount() {
+        this.timer = setTimeout(() =>{
+            console.log('Hiii settimeout Calling')
+        },1000);
+        // console.log('Parent componentDidMount  ')
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timer);
+    }
+
+    render() {
+        // console.log('parent render');
+        return (
+            <div className="about">
+                <h1>This is About Page with class component</h1>
+                <User name="Ravi Kuchhal" city="Noida"/>
+                <UserClass name="Ravi Kuchhal" city="Noida" />
+            </div>
+        );
+
+    }
 }
 export default About;
